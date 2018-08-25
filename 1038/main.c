1f3802791a00b8c9faa43dbe4a810c4cc2647bde
rhikson <rhiksonmedeiros@gmail.com>
2018-08-25 13:54:06 -0300

    Resolução da questão do Lanche

---
+++ 1038/main.c
@@ -0,0 +1,15 @@
+/*
+Lanche: https://www.urionlinejudge.com.br/judge/pt/problems/view/1038
+*/
+#include <stdio.h>
+
+int main (void) {
+  double valores[] = { 4, 4.5, 5, 2, 1.5 };
+  int codigo, quantidade;
+
+  scanf("%d %d", &codigo, &quantidade);
+
+  printf("Total: R$ %.2f\n", valores[codigo - 1] * quantidade);
+
+  return 0;
+}
