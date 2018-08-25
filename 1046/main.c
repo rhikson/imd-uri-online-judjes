a871f01af89418d8d7f7247f5555d61abb080721
rhikson <rhiksonmedeiros@gmail.com>
2018-08-25 16:25:39 -0300

    Resolução da questão do Tempo de jogo

---
+++ 1046/main.c
@@ -0,0 +1,22 @@
+/*
+Tempo de Jogo: https://www.urionlinejudge.com.br/judge/pt/problems/view/1046
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    int comeco, fim, duracao;
+
+    scanf("%d %d", &comeco, &fim);
+
+    if (comeco < fim) {
+        duracao = fim - comeco;
+    }
+    else {
+        duracao = 24 - comeco + fim;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+
+    return 0;
+}
