87d48c7fc4c70ec6ca94f9af3e457cc0d615239f
rhikson <rhiksonmedeiros@gmail.com>
2018-08-25 16:18:24 -0300

    Resolução da questão dos Multiplos

---
+++ 1044/main.c
@@ -0,0 +1,20 @@
+/*
+Múltiplos: https://www.urionlinejudge.com.br/judge/pt/problems/view/1044
+*/
+
+#include <stdio.h>
+
+int main (void) {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a % b == 0 || b % a == 0) {
+        printf("Sao Multiplos\n");
+    }
+    else {
+        printf("Nao sao Multiplos\n");
+    }
+
+    return 0;
+}
