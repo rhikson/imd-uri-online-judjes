b608ef847fb4aa67744f3fc5baf92233fdc14d4c
rhikson <rhiksonmedeiros@gmail.com>
2018-08-27 19:37:04 -0300

    Resolução das questões restantes da lista condicional

---
+++ 1037/main.c
@@ -0,0 +1,32 @@
+/*
+Intervalo: https://www.urionlinejudge.com.br/judge/pt/problems/view/1037
+*/
+
+#include <stdio.h>
+
+int main (void) {
+  double v;
+
+  scanf("%lf", &v);
+
+  if (v < 0) {
+    printf("Fora de intervalo\n");
+  }
+  else if (v <= 25) {
+    printf("Intervalo [0,25]\n");
+  }
+  else if (v <= 50) {
+    printf("Intervalo (25,50]\n");
+  }
+  else if (v <= 75) {
+    printf("Intervalo (50,75]\n");
+  }
+  else if (v <= 100) {
+    printf("Intervalo (75,100]\n");
+  }
+  else {
+    printf("Fora de intervalo\n");
+  }
+
+  return 0;
+}
